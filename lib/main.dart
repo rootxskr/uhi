e3d9e37b1936d111996c3f92a4935ec9460ab44b
Sanskar <skrchaturvedi1@gmail.com>
2025-01-24 14:39:36 +0530

    Log in page

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:uhi/screens/auth_path.dart';
+import 'package:uhi/utils/config.dart';
+import 'main_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This is for push navigator
+  static final navigatorKey = GlobalKey<NavigatorState>();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      navigatorKey: MyApp.navigatorKey, // Use the navigatorKey
+      title: 'UHI',
+      theme: ThemeData(
+        // Pre-defined input decoration
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: Config.primaryColor,
+          border: Config.outlinedBorder,
+          focusedBorder: Config.focusBorder,
+          errorBorder: Config.errorBorder,
+          enabledBorder: Config.outlinedBorder,
+          floatingLabelStyle: TextStyle(color: Config.primaryColor),
+          prefixIconColor: Colors.black38,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Config.primaryColor,
+          selectedItemColor: Colors.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.grey.shade700,
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AuthPage(),
+        '/main': (context) => const MainLayout(), // Add `/` to route name
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("UHI App"),
+      ),
+    );
+  }
+}
