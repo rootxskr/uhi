200563685f1f55f69a2f464795cc97bd9d58a599
Sanskar <skrchaturvedi1@gmail.com>
2025-01-29 12:39:22 +0530

    added Authentication Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:uhi/components/appointment_card.dart';
+import 'package:uhi/components/doctor_card.dart';
+import 'package:uhi/utils/config.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +12,146 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Map<String, dynamic> user = {};
+  Map<String, dynamic> doctor = {};
+  List<dynamic> favList = [];
+  List<Map<String, dynamic>> medCat = [
+    {
+      "icon": FontAwesomeIcons.userDoctor,
+      "category": "General",
+    },
+    {
+      "icon": FontAwesomeIcons.heartPulse,
+      "category": "Cardiology",
+    },
+    {
+      "icon": FontAwesomeIcons.lungs,
+      "category": "Respirations",
+    },
+    {
+      "icon": FontAwesomeIcons.hand,
+      "category": "Dermatology",
+    },
+    {
+      "icon": FontAwesomeIcons.personPregnant,
+      "category": "Gynecology",
+    },
+    {
+      "icon": FontAwesomeIcons.teeth,
+      "category": "Dental",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    Config().init(context);
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15,
+      ),
+        child: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const <Widget>[
+                      Text(
+                        'UserName', //hard core the user's name at first
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage('assets/profile1.jpg'),  //insert Image here
+                        ),
+                      )
+                    ],
+                  ),
+                  Config.spaceMedium,
+                  //category listing
+                  const Text(
+                    'Category',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Config.spaceSmall,
+                  SizedBox(
+                    height: Config.heightSize * 0.05,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children:
+                      List<Widget>.generate(medCat.length, (index) {
+                        return Card(
+                          margin: const EdgeInsets.only(right: 20),
+                          color: Config.primaryColor,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            child: Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                FaIcon(
+                                  medCat[index]['icon'],
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Text(
+                                  medCat[index]['category'],
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                  Config.spaceSmall,
+                  const Text(
+                    'Appointment Today',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Config.spaceSmall,
+                  AppointmentCard(),
+                  Config.spaceSmall,
+                  const Text(
+                    'Top Doctors',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //list of top doctors
+                  //let's build doctors card
+                  Config.spaceSmall,
+                  Column(
+                    children: List.generate(10, (index) {
+                      return const DoctorCard();
+                    }),
+              ),
+                        ],
+                      ),
+            ),
+    ),
+      ),
     );
   }
 }
